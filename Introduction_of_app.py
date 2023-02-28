d0ea0dbd427006799c569292c377d8eae3d9502a
Hrushikeshnettetla <121028431+Hrushikeshnettetla@users.noreply.github.com>
2023-03-01 00:01:12 +0530

    Add files via upload

---
+++ Introduction_of_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("NETTETLA HRUSHIKESH")
+st.subheader("Bsc Data Science Graduate :sunglasses: ")
+st.snow()
+
+btn_click = st.button("Click Me!")
+
+if btn_click == True:
+    st.subheader("Hi,This is my first web app for Data Science Project,thanks for coming here and checking out  :smile:")
+    st.balloons()
+    
